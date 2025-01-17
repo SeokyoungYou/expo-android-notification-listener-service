c4fe6d51b303351dbab1bc9f5a47a637cc37d2a0
Quartz <skyu.dev@gmail.com>
2025-01-17 12:54:13 +0900

    feat: declare the service in manifest file

--- android/src/main/java/expo/modules/androidnotificationlistenerservice/ExpoAndroidNotificationListenerServiceModule.kt
+++ android/src/main/java/expo/modules/androidnotificationlistenerservice/ExpoAndroidNotificationListenerServiceModule.kt
@@ -2,7 +2,8 @@ package expo.modules.androidnotificationlistenerservice
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 
 class ExpoAndroidNotificationListenerServiceModule : Module() {
   override fun definition() = ModuleDefinition {
@@ -11,5 +12,16 @@ class ExpoAndroidNotificationListenerServiceModule : Module() {
     Function("getNotification") {
       "Hello world From Native! 👋"
     } 
+  }
+}
+
+
+class ExpoNotificationListenerService : NotificationListenerService() {
+  override fun onNotificationPosted(sbn: StatusBarNotification) {
+      // 새 알림이 왔을 때 처리
+  }
+
+  override fun onNotificationRemoved(sbn: StatusBarNotification) {
+      // 알림이 제거됐을 때 처리
   }
 }
